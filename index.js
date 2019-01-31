85379a50f7c1ec1ce3901c0a96a7145459f04de8
Nadir Sarigul <nadir.nyc62@gmail.com>
2019-01-31 05:15:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function currentLine(katzDeliLine){
   if (katzDeliLine.length ==0) {
     return "The line is currently empty."
 
-  }
+  }else{
   var myString = ` The line is currently: 1. ${katzDeliLine[0]} `
   for(let i = 1; i<katzDeliLine.length; i++){
     myString+= `, ${i+1}. ${katzDeliLine[i]}`
