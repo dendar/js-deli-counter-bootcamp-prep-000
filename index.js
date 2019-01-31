2f800f19be0a3b99295d65398979454516d6527b
Nadir Sarigul <nadir.nyc62@gmail.com>
2019-01-31 04:57:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,34 +1,42 @@
-var katzDeliLine =[]
-
-function takeANumber(katzDeli, name){
-  line.push(`${name}`)
-  return(`Welcome , ${name}. You are ${katzDeli.lenght} im line.`)
+function takeANumber(katzDeliLine, name){
+  var position =katzDeliLine.lenght+1 
+  katzDeliLine.push(name)
+  return "Welcome, "+name+". You are number" +position+" in line." 
 }
 
-function nowServing(katzDeli){
-  let i =0
-  while(i<katzDeli.length){
-    i++
-  }
-  if(katzDeli.length === 0){
-    return "There is nobody waiting to served!"
-  }
-  else{
-    return(`Currently serving ${katzDeli.shift()}.`)
-  }
 
 
-var line =[]
-
-function currentLine(katzDeli){
-  let i = 0
-  while(i<katzDeli.length){
-    i++
-  }
-  if(katzDeli.length===0){
-    return " there is nobody waiting to be served!"
-  }
-  else{
-    return (`Currently serving ${katzDeli.shift()}.`)
-  }
-}
+// var katzDeliLine =[]
+// 
+// function takeANumber(katzDeli, name){
+//   line.push(`${name}`)
+//   return(`Welcome , ${name}. You are ${katzDeli.lenght} im line.`)
+// }
+// 
+// function nowServing(katzDeli){
+//   let i =0
+//   while(i<katzDeli.length){
+//     i++
+//   }
+//   if(katzDeli.length === 0){
+//     return "There is nobody waiting to served!"
+//   }
+//   else{
+//     return(`Currently serving ${katzDeli.shift()}.`)
+//   }
+// 
+// 
+// var line =[]
+// 
+// function currentLine(katzDeli){
+//   let i = 0
+//   while(i<katzDeli.length){
+//     i++
+//   }
+//   if(katzDeli.length===0){
+//     return " there is nobody waiting to be served!"
+//   }
+//   else{
+//     return (`Currently serving ${katzDeli.shift()}.`)
+//   }
+// }
