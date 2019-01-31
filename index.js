8672ee258ce1b194a3fb963a938aa550b4618c6c
Nadir Sarigul <nadir.nyc62@gmail.com>
2019-01-31 05:01:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 function takeANumber(katzDeliLine, name){
   var position =katzDeliLine.lenght+1
   katzDeliLine.push(name)
-  return "Welcome, "+name+". You are number" +position+" in line." 
+  return "Welcome, "+name+". You are number" +position+" in line."
 }
 
+function nowServing(deliline){
+  var name
+  if (deliline.length ==0) {
+    retun "there is nobody waiting to be served!"
+
+  }
+  name = deliline.shift()
+  return "Currentlyserving " +name+"."
+}
 
 
 // var katzDeliLine =[]
